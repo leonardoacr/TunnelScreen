1cfd27d12cac9f02e31cbd038be90201a2025162
Leonardo Rodrigues <leonardoacrg@gmail.com>
2023-09-10 14:53:53 -0300

    fix: update copy to clipboard function to assure compatibility

--- src/hooks/useCopyToClipboard.ts
+++ src/hooks/useCopyToClipboard.ts
@@ -1,27 +1,36 @@
 import { useState } from "react";
 
 export function useCopyToClipboard() {
-    const [result, setResult] = useState<
-        null | { state: 'success' } | { state: 'error'; message: string }
-    >(null);
+  const [result, setResult] = useState<
+    null | { state: "success" } | { state: "error"; message: string }
+  >(null);
 
-    const copy = async (text: string) => {
-        try {
-            if (text !== '' || null || undefined) {
-                await navigator.clipboard.writeText(text);
-                setResult({ state: 'success' });
-            } else {
-                setResult({ state: 'error', message: 'Please generate an ID to copy' });
-            }
-        } catch (e: any) {
-            setResult({ state: 'error', message: e.message });
-            throw e;
-        } finally {
-            setTimeout(() => {
-                setResult(null);
-            }, 6000);
+  const copy = async (text: string) => {
+    try {
+      if (text !== "" || null || undefined) {
+        if (navigator.clipboard) {
+          await navigator.clipboard.writeText(text);
+        } else {
+          const textArea = document.createElement("textarea");
+          textArea.value = text;
+          document.body.appendChild(textArea);
+          textArea.select();
+          document.execCommand("copy");
+          document.body.removeChild(textArea);
         }
-    };
+        setResult({ state: "success" });
+      } else {
+        setResult({ state: "error", message: "Please generate an ID to copy" });
+      }
+    } catch (e: any) {
+      setResult({ state: "error", message: e.message });
+      throw e;
+    } finally {
+      setTimeout(() => {
+        setResult(null);
+      }, 6000);
+    }
+  };
 
-    return [copy, result] as const;
+  return [copy, result] as const;
 }
